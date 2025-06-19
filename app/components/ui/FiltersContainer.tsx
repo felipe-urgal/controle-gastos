f9dc8d11c123abaceb77c98025c38270d9652671
Felipe Urgal <felipearantesurgal@gmail.com>
2025-06-19 10:47:26 -0300

    melhorias

--- app/components/ui/FiltersContainer.tsx
+++ app/components/ui/FiltersContainer.tsx
@@ -18,48 +18,47 @@ interface FiltersContainerProps {
 const FiltersContainer = ({ children, onClearFilters, message }: FiltersContainerProps) => {
   const [showFilters, setShowFilters] = useState(false);
 
-  const icon = showFilters ? <FaChevronUp size={18}/> : <FaChevronDown size={18}/>
+  const icon = showFilters ? <FaChevronUp/> : <FaChevronDown/>
 
   return (
-    <div className="pt-16">
-      <div className="bg-gray-800 p-3 border-b border-gray-700">
-        <div className="flex flex-col">
-          <div className="flex-1 grid grid-cols-1 lg:grid-cols-4 gap-2 lg:gap-2">
-            {showFilters && (
-              <>
-                {children}
-              </>
-            )}
-          </div>
+    <div className="bg-gray-800 p-3 border-b border-gray-700">
+      <div className="flex flex-col">
+        <div className="flex-1 grid grid-cols-1 lg:grid-cols-4 gap-2 lg:gap-2">
+          {showFilters && (
+            <>
+              {children}
+            </>
+          )}
         </div>
-        <div className="flex justify-end mt-2">
+      </div>
+      <div className="flex justify-end mt-2">
+        <Button
+          onClick={() => setShowFilters(!showFilters)}
+          icon={icon}
+          variant='secondary'
+        >
+          Filtros
+        </Button>
+      </div>
+
+      {message && (
+        <div className="mt-3 flex flex justify-between items-center justify-between bg-yellow-700/30 p-3 rounded-lg my-3">
+          <span className="text-orange-500/70">
+            {message}
+          </span>
           <Button
-            onClick={() => setShowFilters(!showFilters)}
-            icon={icon}
-            size='sm'
-            className="border border-gray-500 text-gray-500 hover:border-orange-500 hover:text-orange-500 cursor-pointer"
+            onClick={() => {
+              onClearFilters();
+              setShowFilters(false);
+            }}
+            icon={<FaTimesCircle size={16} />}
+            className="transition-colors text-orange-500/70 hover:underline cursor-pointer"
           >
-            {showFilters ? 'Esconder' : 'Mostrar'} Filtros
+            Limpar filtros
           </Button>
         </div>
+      )}
 
-        {message && (
-          <div className="mt-3 flex flex justify-between items-center justify-between bg-yellow-700/30 p-3 rounded-lg my-3">
-            <span className="text-orange-500/70">
-              {message}
-            </span>
-            <Button
-              onClick={onClearFilters}
-              icon={<FaTimesCircle size={16} />}
-              size="sm"
-              className="transition-colors text-orange-500/70 hover:underline cursor-pointer"
-            >
-              Limpar filtros
-            </Button>
-          </div>
-        )}
-
-      </div>
     </div>
   );
 };
