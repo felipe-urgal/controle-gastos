1906071663b9682bff558c559a95f4c30903897b
Felipe Urgal <felipearantesurgal@gmail.com>
2025-10-24 13:55:28 -0300

    ajusta

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export const viewport: Viewport = {
   viewportFit: 'cover',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#1F2937' }
+    { media: '(prefers-color-scheme: dark)', color: '#111827' }
   ],
 };
 
