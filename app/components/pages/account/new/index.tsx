7b8b86668862c14efb01ffb042d0ec81a57047b5
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-30 12:24:53 -0300

    feat: add account creation context

--- app/components/pages/account/new/index.tsx
+++ app/components/pages/account/new/index.tsx
@@ -1,13 +1,16 @@
-"use client";
+'use client';
 
-// importing components
-import { NewPage } from "@/app/components/base-pages";
-import { AccountForm } from "@/app/components/pages/account";
+import { NewPage } from '@/app/components/base-pages';
+import { AccountForm } from '@/app/components/pages/account';
 
 export default function New() {
   return (
-    <NewPage backUrl="/contas">
+    <NewPage
+      backUrl="/contas"
+      title="Nova conta"
+      description="Cadastre uma conta para organizar movimentações. O saldo será sempre derivado das transações concluídas."
+    >
       <AccountForm isEditing={false} />
     </NewPage>
   );
-};
+}
