57a3b7f83cbb798719e16c2754e6cc9b231432c0
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-30 11:29:42 -0300

    feat: export mobile topbar

--- app/components/layout/index.ts
+++ app/components/layout/index.ts
@@ -3,4 +3,5 @@ export { default as AppSidebar } from '@/app/components/layout/app-sidebar';
 export { default as BackgroundParticles } from '@/app/components/layout/background-particles';
 export { default as BottomNav } from '@/app/components/layout/bottom-nav';
 export { default as ClientLayout } from '@/app/components/layout/client-layout';
+export { default as MobileTopbar } from '@/app/components/layout/mobile-topbar';
 export { default as ProtectedRoute } from '@/app/components/layout/protected-route';
