9ebffa5d15da3938db6668440a0e194294184e8b
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-06 12:48:15 -0300

    refactor(dashboard): apontar entrypoint para implementação Orbit estável

--- app/components/pages/dashboard/dashboard/index.tsx
+++ app/components/pages/dashboard/dashboard/index.tsx
@@ -1,3 +1,3 @@
 'use client';
 
-export { default } from '@/app/components/pages/dashboard/dashboard/orbit-dashboard';
+export { default } from '@/app/components/pages/dashboard/dashboard/orbit-dashboard-v2';
