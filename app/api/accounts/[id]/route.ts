502a5b43515c942ea89a161bf9efd38811b0af63
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-12 10:23:14 -0300

    refactor(accounts): consolidar CRUD no domínio

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -1,4 +1,4 @@
-import { accountCrud } from "@/app/lib/crud/account.crud";
+import { accountCrud } from "@/app/lib/accounts/account-crud";
 
 export const GET = accountCrud.getById;
 export const PUT = accountCrud.update;
